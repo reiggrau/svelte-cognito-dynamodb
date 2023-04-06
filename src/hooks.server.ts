d7fb14d1c2f8fb2f122410d9cac1f1e0d725b64c
guillemreig <guillemreiggrau@gmail.com>
2023-04-06 16:21:23 +0200

    Login not working with Cognito

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,28 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/core/providers/google';
+import Cognito from '@auth/core/providers/cognito';
 
 import type { OAuthConfig } from '@auth/core/providers/index.js';
 import type { Profile } from '@auth/core/types.js';
 
-import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
+import {
+	GOOGLE_ID,
+	GOOGLE_SECRET,
+	COGNITO_ID,
+	COGNITO_SECRET,
+	COGNITO_ISSUER
+} from '$env/static/private';
 
 export const handle = SvelteKitAuth({
 	providers: [
-		Google({
-			clientId: GOOGLE_ID,
-			clientSecret: GOOGLE_SECRET
+		// Google({
+		// 	clientId: GOOGLE_ID,
+		// 	clientSecret: GOOGLE_SECRET
+		// }) as OAuthConfig<Profile>,
+		Cognito({
+			clientId: COGNITO_ID,
+			clientSecret: COGNITO_SECRET,
+			issuer: COGNITO_ISSUER
 		}) as OAuthConfig<Profile>
 	]
 });
